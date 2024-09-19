7fa7e5f76e8544df9baaa41d042f7122e8671afc
manyapatkar974 <manya.23cs070@sode-edu.in>
2024-09-19 10:33:06 +0530

    Update and rename K89.java to K11.java

--- K11.java
+++ K11.java
@@ -26,7 +26,7 @@ class Stack{
 System.out.println(stk[i]);}}}}
      
        
-public class K89{
+public class K11{
   public static void main(String[] args){
     int ch;
     Stack stack1=new Stack();
